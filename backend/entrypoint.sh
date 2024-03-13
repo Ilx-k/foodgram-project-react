999b7a82063c31ec109f5fb0409de9aa715256da
Стас Басов <stas.basov@whatever.com>
2024-03-14 02:01:23 +0300

    stage 2.3

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,6 @@
 python manage.py collectstatic --noinput
 cp -r /app/static/. /backend_static/
-cp -r /app/media/. /backend_media/
+cp -r /app/media/. /media/
 python manage.py migrate
 python manage.py import_csv
 gunicorn foodgram.wsgi:application --bind 0:8000
