92a12e1afa5944bf96aa6b3a2bf70f85ad9a9601
Стас Басов <stas.basov@whatever.com>
2024-04-14 20:51:54 +0300

    review3.6.9.23

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.serializers import SetPasswordSerializer
 from djoser.views import UserViewSet
-from rest_framework import filters, status, viewsets
+from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import (IsAuthenticated,
