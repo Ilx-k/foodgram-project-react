72fc3f526f45328f917dde5fdc2b9fe1a516a127
Стас Басов <stas.basov@whatever.com>
2024-04-14 20:03:10 +0300

    review3.6.9.18

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -7,6 +7,11 @@ class IsAuthenticatedOrReadOnly(BasePermission):
         return request.method in SAFE_METHODS or (
             request.user and request.user.is_authenticated)
 
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in SAFE_METHODS
+            or obj.author == request.user)
+
 
 class IsAdminUserOrReadOnly(BasePermission):
 
