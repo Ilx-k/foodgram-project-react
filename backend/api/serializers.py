1e565bf8671ed292a1d2ad88d0236843b01157fe
Стас Басов <stas.basov@whatever.com>
2024-04-15 00:25:11 +0300

    review3.6.9.42

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -351,21 +351,41 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
             'cooking_time': {'required': True},
         }
 
-    def validate(self, obj):
+    def validate(self, data):
         for field in ['name', 'text', 'cooking_time']:
-            if not obj.get(field):
+            if not data.get(field):
                 raise serializers.ValidationError(
                     f'{field} - Обязательное поле.'
                 )
-        if not obj.get('tags'):
+        if not data.get('tags'):
             raise serializers.ValidationError(
                 'Нужно указать минимум 1 тег.'
             )
-        if not obj.get('ingredients'):
+        tags = self.initial_data.get('tags')
+        lst_tags = []
+
+        for tag in tags:
+            if tag['id'] in lst_tags:
+                raise serializers.ValidationError(
+                    'Тэги должны быть уникальными!'
+                )
+            lst_tags.append(tag['id'])
+
+        if not data.get('ingredients'):
             raise serializers.ValidationError(
                 'Нужно указать минимум 1 ингредиент.'
             )
-        return obj
+        ingredients = self.initial_data.get('ingredients')
+        lst_ingredient = []
+        for ingredient in ingredients:
+            if ingredient['id'] in lst_ingredient:
+                raise serializers.ValidationError(
+                    'Ингредиенты должны быть уникальными!'
+                )
+            lst_ingredient.append(ingredient['id'])
+
+
+        return data
 
     def validate_cooking_time(self, value):
         if int(value) < 1:
