afc96321fa76a530d84b5b6049f3237810015f21
Стас Басов <stas.basov@whatever.com>
2024-04-14 21:04:02 +0300

    review3.6.9.24

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -274,16 +274,6 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
     tags = serializers.PrimaryKeyRelatedField(many=True,
                                               queryset=Tag.objects.all(),
                                               required=True)
-    cooking_time = serializers.IntegerField(
-        min_value=SCORE_MIN,
-        max_value=SCORE_MAX,
-        error_messages={
-            "min_value":
-            f"Время приготовления не может быть меньше {SCORE_MIN} минуты.",
-            "max_value":
-            f"Время приготовления не может быть больше {SCORE_MIN} минут."
-        }
-    )
 
     class Meta:
         model = Recipe
@@ -295,6 +285,50 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
             'text',
             'cooking_time',
         )
+        extra_kwargs = {
+            'ingredients': {'required': True, 'allow_blank': False},
+            'tags': {'required': True, 'allow_blank': False},
+            'name': {'required': True, 'allow_blank': False},
+            'image': {'required': True, 'allow_blank': False},
+            'text': {'required': True, 'allow_blank': False},
+            'cooking_time': {'required': True},
+        }
+
+    def validate(self, data):
+        ingredients = data.get('ingredients')
+        if not ingredients:
+            raise serializers.ValidationError(
+                {"ingredients": "Поле ингредиентов не может быть пустым!"}
+            )
+        if (len(set(item['id'] for item in ingredients)) != len(ingredients)):
+            raise serializers.ValidationError(
+                'Ингридиенты не должны повторяться!')
+        tags = data.get('tags')
+        if not tags:
+            raise serializers.ValidationError(
+                {"tags": "Поле тегов не может быть пустым!"}
+            )
+        if len(set(tags)) != len(tags):
+            raise serializers.ValidationError(
+                {"tags": "Теги не должны повторяться!"}
+            )
+        return data
+
+    def validate_cooking_time(self, value):
+        if int(value) < SCORE_MIN:
+            raise serializers.ValidationError(
+                'Время готовки не должно быть меньше минуты')
+        if int(value) > SCORE_MAX:
+            raise serializers.ValidationError(
+                'Время готовки не должно быть больше суток')
+        return value
+
+    def validate_image(self, image):
+        if not image:
+            raise serializers.ValidationError(
+                {"image": "Поле изображения не может быть пустым!"}
+            )
+        return image
 
     @atomic(durable=True)
     def create(self, validated_data):
